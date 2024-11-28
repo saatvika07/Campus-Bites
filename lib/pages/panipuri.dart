e9c6407f4c03e45a929356041f9b8e1fb5cbe933
saatvika07 <22wh1a6628@bvrithyderabad.edu.in>
2024-11-29 01:22:59 +0530

    added search widget
    for access of search widget in each of the files

--- lib/pages/panipuri.dart
+++ lib/pages/panipuri.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:login_signup/widgets/custom_scaffold.dart';
+import 'package:login_signup/widgets/search_widget.dart';
 
 class PaniPuriPage extends StatelessWidget {
   const PaniPuriPage({super.key});
@@ -9,46 +10,8 @@ class PaniPuriPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return CustomScaffold(
       child: ListView(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 10,
-              horizontal: 15,
-            ),
-            child: Container(
-              width: double.infinity,
-              height: 50,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 2,
-                    blurRadius: 10,
-                    offset: const Offset(0,3),
-                  ),
-                ]
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 10,
-                ),
-                child: Row(children: [
-                  const Icon(
-                    CupertinoIcons.search,
-                    color: Colors.orange
-                    ),
-                  Container(
-                    height: 50,
-                    width: 300,
-                    child: const Padding(padding: EdgeInsets.symmetric(
-                      horizontal: 15,
-                    ))
-                  )
-                  
-                ],)),
-            ))
+        children: const [
+          SearchWidget()
         ],
       )
 
